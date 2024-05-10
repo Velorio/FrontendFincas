f458390e894ba6d3f5e33212c8d42179213e16ea
Jerson Osorio <61725276+Velorio@users.noreply.github.com>
2024-05-10 11:28:00 -0500

    listar propiedades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,10 @@ import { CrearArrendadorComponent } from './components/crear-arrendador/crear-ar
 import { ListarArrendadoresComponent } from './components/listar-arrendadores/listar-arrendadores.component';
 import { CrearArrendatarioComponent } from './components/crear-arrendatario/crear-arrendatario.component';
 import { ListarArrendatariosComponent } from './components/listar-arrendatarios/listar-arrendatarios.component';
+import { MejoresPropiedadesComponent } from './components/mejores-propiedades/mejores-propiedades.component';
 
 export const routes: Routes = [
+    { path: '', component: MejoresPropiedadesComponent},
     { path: 'pruebaPost', component: CrearPropiedadComponent},
     { path: 'pruebaGet', component: ListarPropiedadesComponent},
     { path: 'Arrendadores/nuevo', component: CrearArrendadorComponent},
